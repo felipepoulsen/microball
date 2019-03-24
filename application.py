b6a164cfc7b46d6cd216b82f0697bec5f889a53a
Felipe Poulsen <felipepoulsen@Felipes-MacBook-Pro.local>
2019-03-24 10:27:04 +0100

    initial commit

---
+++ application.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+from flask_restplus import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+matches = {}
+
+@api.route('/<string:match_id>')
+class LogMatches(Resource):
+    def get(self, match_id):
+        return {match_id: matches[match_id]}
+
+    def put(self, match_id):
+        matches[match_id] = request.form['data']
+        return {match_id: matches[match_id]}
+
+if __name__ == '__main__':
+    app.run(debug=True)
