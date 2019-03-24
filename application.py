08d87c8af5d56d05cc57b93c8f00958163996d0a
Felipe Poulsen <felipepoulsen@Felipes-MacBook-Pro.local>
2019-03-24 11:03:39 +0100

    changed key to result

--- application.py
+++ application.py
@@ -12,7 +12,7 @@ class LogMatches(Resource):
         return {match_id: matches[match_id]}
 
     def put(self, match_id):
-        matches[match_id] = request.form['data']
+        matches[match_id] = request.form['result']
         return {match_id: matches[match_id]}
 
 if __name__ == '__main__':
